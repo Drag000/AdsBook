12f53d21668007eb12d22527b6aa334fba965ee0
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-01-07 15:51:18 +0200

    Fix entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 echo "Waiting for PostgreSQL to be ready..."
 # Checks if the PostgreSQL service on DB_HOST (=test_db_container) is reachable on port 5432.
 # If the check fails, the script waits for 1 second before retrying.
-while ! nc -z $DB_HOST 5432; do
+while ! nc -z "$DB_HOST" 5432; do
   sleep 1
 done
 echo "PostgreSQL is up and running!"
